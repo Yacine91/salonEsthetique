87b13faa0a3db35c13c3660570e989b4268c1259
Yacine Slimani <slimani.yacine91@outlook.com>
2019-02-07 15:52:58 +0100

    Ajout des component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PrestationComponent } from './prestation/prestation.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'prestation', component: PrestationComponent},
+  {path: 'pictures', component: PicturesComponent},
+  {path: 'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PrestationComponent,
+    PicturesComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
