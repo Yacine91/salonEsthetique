d355cd46dbb74c17d014c765ad0c4ff727f54b56
TLATI <bestsoft.93@gmail.com>
2019-02-11 16:13:25 +0100

    rendez-vous
    rendez-vous avec prestation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PrestationComponent } from './prestation/prestation.component';
 import { PicturesComponent } from './pictures/pictures.component';
 import { ContactComponent } from './contact/contact.component';
+import { RendezVousComponent } from './rendez-vous/rendez-vous.component';
+import {HttpModule} from '@angular/http';
 
 const routes = [
   {path: 'home', component: HomeComponent},
   {path: 'prestation', component: PrestationComponent},
   {path: 'pictures', component: PicturesComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: 'rendez-vous',component:RendezVousComponent}
 ]
 
 @NgModule({
@@ -21,11 +26,14 @@ const routes = [
     HomeComponent,
     PrestationComponent,
     PicturesComponent,
-    ContactComponent
+    ContactComponent,
+    RendezVousComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
